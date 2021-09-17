d953bfe0bf6458391b7750076e3fcd00c40db01b
satoru02 <sugiwaka02@gmail.com>
2021-09-17 17:14:52 +0900

    #2 [unfinished] access theater db

--- handler.js
+++ handler.js
@@ -41,19 +41,27 @@ module.exports.main = async (event) => {
       case "unfollow":
         break;
       case "postback":
+        if(body.postback.data === "datePick"){
+          var res = await getUser(userId);
+          var user = JSON.parse(res);
+          var messages = await setReply(user, body);
+          if (messages.length > 0) {
+            await replyMessage(replyToken, messages);
+          }
+        }
         break;
     }
   }
 
   async function setReply(user, eventBody) {
     var messages = [];
-    var messageType = eventBody.message.type;
     switch (user.situation) {
       case "address":
+        var messageType = eventBody.message.type;
         if (messageType != "text" && messageType != "location") {
           return messages;
         }
-        
+
         var keyParams = {
           "user_id": user.user_id,
         }
@@ -63,8 +71,9 @@ module.exports.main = async (event) => {
           ":lng": "",
           ":lat": "",
           ":plc": "",
+          ":sct": "",
           ":dir": 0,
-          ":rel": 0
+          ":rel": 0,
         }
         await updateUser(keyParams, expAtr);
 
@@ -96,6 +105,7 @@ module.exports.main = async (event) => {
             ":lng": location.lng,
             ":lat": location.lat,
             ":plc": destination,
+            ":sct": "",
             ":dir": 0,
             ":rel": 0
           }
@@ -110,59 +120,71 @@ module.exports.main = async (event) => {
           messages.push(errorMsg);
         }
         break;
-        // ここでcnmDBを叩く。
-        // 時間をテキストで受けるかquickReplayで受けるか？
-        // ①すでに保存したuserの地理情報から、近くの映画館取得
-        // ②目的地の映画館の上映時間から、指定された時間からキュレーション
-        // case "time":
-        // case "searched":
-        //   if (messageType != "text"){
-        //     return messages;
-        //   }
-        //   var time = eventBody.message.text;
-        //   time = time.replace(/\r?\n/g, " ");
-        //   time = time.trim();
-        //   if(time == "なし"){
-        //     time = "";
-        //   }
-        //   // ユーザ情報の検索結果クリア
-        //   // clearSearchResult(userRow);
-        //   // ユーザ情報に目的時間を保存
-        //   // setSearchKeyword(userRow, time);
-        //   // ユーザーの地理からcnmDBから映画館取得
-        //   var placeList = new Array();
-        //   var ret1 = getPlace(location.lat,location.lng,keyword);
-        //   if(typeof(ret1) == "object"){
-        //     placeList = ret1;
-        //     searchStatus = "OK";
-        //   } else {
-        //     searchStatus = ret1;
-        //   }
-        //   if(searchStatus == "OK"){
-        //     setSearchResult(userRow, placeList);
-        //     setUserStatus(userRow, "searched");
-        //     var msg1 = searchFinishMessage(userRow);
-        //     messages.push(msg1);
-        //     var msg2 = placesMessage(userRow);
-        //     messages.push(msg2);
-        //   } else if(searchStatus == "ZERO_RESULTS") {
-        //     var msg1 = noTheaterMessage(userRow);
-        //     messages.push(msg1);
-        //     //最初に戻る
-        //     setUserStatus(userRow, "address");
-        //   } else {
-        //      var msg1 = googleErrorMessage("【Places API】", searchStatus);
-        //      messages.push(msg1);
-        //     //最初に戻る
-        //      setUserStatus(userRow, "address");
-        //   }
-        //   break;
+        case "time":
+        case "searched":
+          if(eventBody.message){
+            var resetTimeMsg = resetTimeMessage();
+            messages.push(resetTimeMsg);
+            return messages;
+          } else if(eventBody.type == "postback") {
+            var keyParams = {
+              "user_id": user.user_id,
+            }
+            var expAtr = {
+              ":sit": "searched",
+              ":sct": eventBody.postback.params.time,
+              ":lng": user.lng,
+              ":lat": user.lat,
+              ":plc": user.place,
+              ":dir": 0,
+              ":rel": 0
+            }
+            await updateUser(keyParams, expAtr);
+          }
+
+          var params = {
+            lat: user.lat,
+            lng: user.lng,
+            place: user.place
+          }
+
+          var theaterList = new Array();
+          var theaters = getTheaterInfo(params)
+
+          // var ret1 = getPlace(location.lat,location.lng,keyword);
+          // if(typeof(ret1) == "object"){
+          //   placeList = ret1;
+          //   searchStatus = "OK";
+          // } else {
+          //   searchStatus = ret1;
+          // }
+          // if(searchStatus == "OK"){
+          //   setSearchResult(userRow, placeList);
+          //   setUserStatus(userRow, "searched");
+          //   var msg1 = searchFinishMessage(userRow);
+          //   messages.push(msg1);
+          //   var msg2 = placesMessage(userRow);
+          //   messages.push(msg2);
+          // } else if(searchStatus == "ZERO_RESULTS") {
+          //   var msg1 = noTheaterMessage(userRow);
+          //   messages.push(msg1);
+          //   //最初に戻る
+          //   setUserStatus(userRow, "address");
+          // } else {
+          //    var msg1 = googleErrorMessage("【Places API】", searchStatus);
+          //    messages.push(msg1);
+          //   //最初に戻る
+          //    setUserStatus(userRow, "address");
+          // }
+          // break;
     }
     return messages;
   }
 };
 
 async function getLocation(address) {
+  var geocodeApiUrl = "https://maps.googleapis.com/maps/api/geocode/json";
+  var location = new Object();
   var params = {
     address: address,
     components: {
@@ -170,10 +192,7 @@ async function getLocation(address) {
     },
     key: googleKey,
   };
-  var geocodeApiUrl = "https://maps.googleapis.com/maps/api/geocode/json";
-  var location = new Object();
-
-  await axios.get(geocodeApiUrl, {
+  location = await axios.get(geocodeApiUrl, {
       params: params
     })
     .then((res) => {
@@ -182,7 +201,6 @@ async function getLocation(address) {
       return location;
     })
     .catch((err) => {
-      console.log(err)
       return "ZERO_RESULTS";
     });
     return location;
@@ -213,12 +231,47 @@ function resetAddressMessage() {
 }
 
 function setTimeMessage() {
-  var text = {
+  var msg = {
     "type": "text",
-    "text": "何時くらいに観に行きますか？"
+    "text": "何時くらいに観に行きますか？下のボタンから選んでね。",
+    "quickReply": {
+      "items" : [
+        {
+          "type": "action",
+          "action": {
+            "type" : "datetimepicker",
+            "label": "Select date",
+            "data": "datePick",
+            "mode": "time"
+          }
+        }
+      ]
+    }
   };
-  return text;
+  return msg;
 }
+
+function resetTimeMessage() {
+  var msg = {
+    "type": "text",
+    "text": "ごめんなさい!時間は下のボタンから選んでね。",
+    "quickReply": {
+      "items" : [
+        {
+          "type": "action",
+          "action": {
+            "type" : "datetimepicker",
+            "label": "Select date",
+            "data": "datePick",
+            "mode": "time"
+          }
+        }
+      ]
+    }
+  };
+  return msg;
+}
+
 
 // function noTheaterMessage(row) {
 //   return msg;
@@ -228,11 +281,13 @@ function setTimeMessage() {
 //   return msg;
 // }
 
-// function placesMessage(row) {
+// function theatersMessage(row) {
 //   return msg;
 // }
 
-function googleErrorMessage(row) {}
+// function googleErrorMessage(row) {
+// //   return msg;
+// }
 
 async function replyMessage(replyToken, messages) {
   await bot.reply(replyToken, messages)
@@ -241,6 +296,18 @@ async function replyMessage(replyToken, messages) {
     }).catch(function (error) {
       console.log('Error', error);
     });
+}
+
+async function getTheaterInfo(params){
+  var params = params
+  var res = await docClient.get(params, function (err, data) {
+    if (err) {
+      console.error("Unable to create user. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+      console.log("Created user:", JSON.stringify(data, null, 2));
+    }
+  }).promise();
+  console.log(res);
 }
 
 async function createUser(userId) {
@@ -282,7 +349,7 @@ async function updateUser(keyParams, expAtr) {
   var params = {
     TableName: "eigabot_users",
     Key: keyParams,
-    UpdateExpression: "set situation = :sit, lng = :lng, lat = :lat, place = :plc, displayResults = :dir, results = :rel",
+    UpdateExpression: "set situation = :sit, lng = :lng, lat = :lat, place = :plc, displayResults = :dir, results = :rel, scheduledTime = :sct",
     ExpressionAttributeValues: expAtr,
     ReturnValues:"UPDATED_NEW"
   };
